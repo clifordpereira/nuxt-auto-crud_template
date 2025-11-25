cd1a2457ca061ec3355f25fc8bc19b7ae74365cd
Cliford Pereira <cliford.pereira@gmail.com>
2025-11-25 22:04:55 +0530

    feat: Implement Drizzle ORM for database management and integrate NuxtHub, removing the CI workflow.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/ui'
-  ],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxthub/core", "@nuxt/image"],
 
   devtools: {
-    enabled: true
+    enabled: true,
   },
 
-  css: ['~/assets/css/main.css'],
+  css: ["~/assets/css/main.css"],
 
   routeRules: {
-    '/': { prerender: true }
+    "/": { prerender: true },
   },
 
-  compatibilityDate: '2025-01-15',
-
-  eslint: {
-    config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
-    }
-  }
-})
+  compatibilityDate: "2025-01-15",
+  hub: {
+    // NuxtHub options
+  },
+});
