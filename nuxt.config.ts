68fc39da0966d7ac4cea19b5f09a0885425774d0
Cliford Pereira <cliford.pereira@gmail.com>
2025-11-27 17:57:47 +0530

    feat: Add nuxt-auto-crud module and its dependencies.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxthub/core", "@nuxt/image"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxthub/core", "@nuxt/image", "nuxt-auto-crud"],
 
   devtools: {
     enabled: true,
